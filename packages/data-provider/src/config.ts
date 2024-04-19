797768b68baa35036971cdf1f4f752e18ec7661a
Danny Avila <danny@librechat.ai>
2024-04-19 12:05:39 -0400

    🧼 refactor(AppService): Consolidate Logic & Issue more Warnings (#2468)
    * chore: bump example config version
    
    * refactor(AppService): issue warnings from separate modules where possible
    
    * refactor(AppService): consolidate AppService logic to separate modules as much as possible
    
    * chore: bump data-provider
    
    * chore: remove unn. variable definition
    
    * chore: warning wording

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import { z } from 'zod';
+import type { ZodError } from 'zod';
 import { EModelEndpoint, eModelEndpointSchema } from './schemas';
 import { fileConfigSchema } from './file-config';
 import { FileSources } from './types/files';
@@ -82,6 +83,11 @@ export type TValidatedAzureConfig = {
   modelNames: string[];
   modelGroupMap: TAzureModelGroupMap;
   groupMap: TAzureGroupMap;
+};
+
+export type TAzureConfigValidationResult = TValidatedAzureConfig & {
+  isValid: boolean;
+  errors: (ZodError | string)[];
 };
 
 export enum Capabilities {
@@ -176,7 +182,7 @@ export const azureEndpointSchema = z
   );
 
 export type TAzureConfig = Omit<z.infer<typeof azureEndpointSchema>, 'groups'> &
-  TValidatedAzureConfig;
+  TAzureConfigValidationResult;
 
 export const rateLimitSchema = z.object({
   fileUploads: z
