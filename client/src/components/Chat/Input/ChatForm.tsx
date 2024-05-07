8ac749380d9dad9232583775ee4bfff980ab6f7b
Danny Avila <danny@librechat.ai>
2024-05-07 13:13:55 -0400

    📧 feat: Mention "@" Command Popover (#2635)
    * feat: initial mockup
    
    * wip: activesetting, may use or not use
    
    * wip: mention with useCombobox usage
    
    * feat: connect textarea to new mention popover
    
    * refactor: consolidate icon logic for Landing/convos
    
    * refactor: cleanup URL logic
    
    * refactor(useTextarea): key up handler
    
    * wip: render desired mention options
    
    * refactor: improve mention detection
    
    * feat: modular chat the default option
    
    * WIP: first pass mention selection
    
    * feat: scroll mention items with keypad
    
    * chore(showMentionPopoverFamily): add typing to atomFamily
    
    * feat: removeAtSymbol
    
    * refactor(useListAssistantsQuery): use defaultOrderQuery as default param
    
    * feat: assistants mentioning
    
    * fix conversation switch errors
    
    * filter mention selections based on startup settings and available endpoints
    
    * fix: mentions model spec icon URL
    
    * style: archive icon
    
    * fix: convo renaming behavior on click
    
    * fix(Convo): toggle hover state
    
    * style: EditMenu refactor
    
    * fix: archive chats table
    
    * fix: errorsToString import
    
    * chore: remove comments
    
    * chore: remove comment
    
    * feat: mention descriptions
    
    * refactor: make sure continue hover button is always last, add correct fork button alt text

--- client/src/components/Chat/Input/ChatForm.tsx
+++ client/src/components/Chat/Input/ChatForm.tsx
@@ -17,6 +17,7 @@ import { mainTextareaId } from '~/common';
 import StopButton from './StopButton';
 import SendButton from './SendButton';
 import FileRow from './Files/FileRow';
+import Mention from './Mention';
 import store from '~/store';
 import { chatConstants } from 'librechat-data-provider';
 import { useState } from 'react';
@@ -25,6 +26,9 @@ const ChatForm = ({ index = 0 }) => {
   const submitButtonRef = useRef<HTMLButtonElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
   const [showStopButton, setShowStopButton] = useRecoilState(store.showStopButtonByIndex(index));
+  const [showMentionPopover, setShowMentionPopover] = useRecoilState(
+    store.showMentionPopoverFamily(index),
+  );
   const { requiresKey } = useRequiresKey();
   const [isOverLimit, setIsOverLimit] = useState(false);
 
@@ -32,11 +36,12 @@ const ChatForm = ({ index = 0 }) => {
     defaultValues: { text: '' },
   });
 
-  const { handlePaste, handleKeyDown, handleCompositionStart, handleCompositionEnd } = useTextarea({
-    textAreaRef,
-    submitButtonRef,
-    disabled: !!requiresKey,
-  });
+  const { handlePaste, handleKeyDown, handleKeyUp, handleCompositionStart, handleCompositionEnd } =
+    useTextarea({
+      textAreaRef,
+      submitButtonRef,
+      disabled: !!requiresKey,
+    });
 
   const {
     ask,
@@ -107,6 +112,9 @@ const ChatForm = ({ index = 0 }) => {
       >
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="flex w-full items-center">
+            {showMentionPopover && (
+              <Mention setShowMentionPopover={setShowMentionPopover} textAreaRef={textAreaRef} />
+            )}
             <div className="[&:has(textarea:focus)]:border-token-border-xheavy border-token-border-medium bg-token-main-surface-primary relative flex w-full flex-grow flex-col overflow-hidden rounded-2xl border dark:border-gray-600 dark:text-white [&:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] dark:[&:has(textarea:focus)]:border-gray-500">
               <FileRow
                 files={files}
@@ -129,6 +137,7 @@ const ChatForm = ({ index = 0 }) => {
                   disabled={disableInputs}
                   onPaste={handlePaste}
                   onKeyDown={handleKeyDown}
+                  onKeyUp={handleKeyUp}
                   onCompositionStart={handleCompositionStart}
                   onCompositionEnd={handleCompositionEnd}
                   id={mainTextareaId}
--- client/src/components/Chat/Landing.tsx
+++ client/src/components/Chat/Landing.tsx
@@ -2,11 +2,10 @@ import { EModelEndpoint } from 'librechat-data-provider';
 import { useGetEndpointsQuery, useGetStartupConfig } from 'librechat-data-provider/react-query';
 import type { ReactNode } from 'react';
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '~/components/ui';
-import { getEndpointField, getIconEndpoint, getIconKey } from '~/utils';
 import { useChatContext, useAssistantsMapContext } from '~/Providers';
-import ConvoIconURL from '~/components/Endpoints/ConvoIconURL';
-import { icons } from './Menus/Endpoints/Icons';
+import ConvoIcon from '~/components/Endpoints/ConvoIcon';
 import { BirthdayIcon } from '~/components/svg';
+import { getIconEndpoint, cn } from '~/utils';
 import { useLocalize } from '~/hooks';
 
 export default function Landing({ Header }: { Header?: ReactNode }) {
@@ -31,91 +30,74 @@ export default function Landing({ Header }: { Header?: ReactNode }) {
   const iconURL = conversation?.iconURL;
   endpoint = getIconEndpoint({ endpointsConfig, iconURL, endpoint });
 
-  const endpointIconURL = getEndpointField(endpointsConfig, endpoint, 'iconURL');
-  const iconKey = getIconKey({ endpoint, endpointsConfig, endpointIconURL });
-  const Icon = icons[iconKey];
-
   const assistant = endpoint === EModelEndpoint.assistants && assistantMap?.[assistant_id ?? ''];
   const assistantName = (assistant && assistant?.name) || '';
   const assistantDesc = (assistant && assistant?.description) || '';
   const avatar = (assistant && (assistant?.metadata?.avatar as string)) || '';
 
-  let className =
+  const containerClassName =
     'shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black';
 
-  if (assistantName && avatar) {
-    className = 'shadow-stroke overflow-hidden rounded-full';
-  }
-
   return (
-    <div className="relative h-full">
-      <div className="absolute left-0 right-0">{Header && Header}</div>
-      <div className="flex h-full flex-col items-center justify-center">
-        <div className="mb-3 h-[72px] w-[72px]">
-          <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-[#FFB400]/95 text-black">
-            {icons[endpoint ?? 'unknown']({ size: 41, className: 'h-2/3 w-2/3' })}
-            {/*
-              <TooltipProvider delayDuration={50}>
-                <Tooltip>
-                  <div className="relative h-full">
-                    <div className="absolute left-0 right-0">{Header && Header}</div>
-                    <div className="flex h-full flex-col items-center justify-center">
-                      <div className="relative mb-3 h-[72px] w-[72px]">
-                        {iconURL && iconURL.includes('http') ? (
-                          <ConvoIconURL
-                            preset={conversation}
-                            endpointIconURL={endpointIconURL}
-                            assistantName={assistantName}
-                            assistantAvatar={avatar}
-                            context="landing"
-                          />
-                        ) : (
-                          <div className={className}>
-                            {endpoint &&
-                              Icon &&
-                              Icon({
-                                size: 41,
-                                context: 'landing',
-                                className: 'h-2/3 w-2/3',
-                                iconURL: endpointIconURL,
-                                assistantName,
-                                endpoint,
-                                avatar,
-                              })}
-                          </div>
-                        )}
-                        <TooltipTrigger>
-                          {(startupConfig?.showBirthdayIcon ?? false) && (
-                            <BirthdayIcon className="absolute bottom-12 right-5" />
-                          )}
-                        </TooltipTrigger>
-                        <TooltipContent side="top" sideOffset={115} className="left-[20%]">
-                          {localize('com_ui_happy_birthday')}
-                        </TooltipContent>
-                      </div>
-                      {assistantName ? (
-                        <div className="flex flex-col items-center gap-0 p-2">
-                          <div className="text-center text-2xl font-medium dark:text-white">
-                            {assistantName}
-                          </div>
-                          <div className="text-token-text-secondary max-w-md text-center text-xl font-normal ">
-                            {assistantDesc ? assistantDesc : localize('com_nav_welcome_message')}
-                          </div>
-                        </div>
-                      ) : (
-                        <div className="mb-5 text-2xl font-medium dark:text-white">
-                          {localize('com_nav_welcome_message')}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </Tooltip>
-              </TooltipProvider>
-            */}
+  // <div className="relative h-full">
+  //   <div className="absolute left-0 right-0">{Header && Header}</div>
+  //   <div className="flex h-full flex-col items-center justify-center">
+  //     <div className="mb-3 h-[72px] w-[72px]">
+  //       <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-[#FFB400]/95 text-black">
+  //         {icons[endpoint ?? 'unknown']({ size: 41, className: 'h-2/3 w-2/3' })}
+
+    <TooltipProvider delayDuration={50}>
+      <Tooltip>
+        <div className="relative h-full">
+          <div className="absolute left-0 right-0">{Header && Header}</div>
+          <div className="flex h-full flex-col items-center justify-center">
+            <div
+              className={cn(
+                'relative h-[72px] w-[72px]',
+                assistantName && avatar ? 'mb-0' : 'mb-3',
+              )}
+            >
+              <ConvoIcon
+                conversation={conversation}
+                assistantMap={assistantMap}
+                endpointsConfig={endpointsConfig}
+                containerClassName={containerClassName}
+                context="landing"
+                className="h-2/3 w-2/3"
+                size={41}
+              />
+              <TooltipTrigger>
+                {(startupConfig?.showBirthdayIcon ?? false) && (
+                  <BirthdayIcon className="absolute bottom-12 right-5" />
+                )}
+              </TooltipTrigger>
+              <TooltipContent side="top" sideOffset={115} className="left-[20%]">
+                {localize('com_ui_happy_birthday')}
+              </TooltipContent>
+            </div>
+            {assistantName ? (
+              <div className="flex flex-col items-center gap-0 p-2">
+                <div className="text-center text-2xl font-medium dark:text-white">
+                  {assistantName}
+                </div>
+                <div className="text-token-text-secondary max-w-md text-center text-xl font-normal ">
+                  {assistantDesc ? assistantDesc : localize('com_nav_welcome_message')}
+                </div>
+              </div>
+            ) : (
+              <div className="mb-5 text-2xl font-medium dark:text-white">
+                {localize('com_nav_welcome_message')}
+              </div>
+            )}
           </div>
         </div>
-        <div className="text-center text-xl font-medium dark:text-white">Go Bruins!</div>
-      </div>
-    </div>
+      </Tooltip>
+    </TooltipProvider>
+
+  //       </div >
+  //     </div >
+  //     <div className="text-center text-xl font-medium dark:text-white">Go Bruins!</div>
+  //   </div >
+  // </div >
   );
 }
